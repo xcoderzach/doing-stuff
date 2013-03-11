0b61e037218dd8c9ebe6ccc10126270e4851d2cc
Zach Smith <zach@amicushq.com>
2013-03-10 21:02:18 -0400

    handle += assignments and stuff

--- awesome.js
+++ awesome.js
@@ -4,7 +4,11 @@ var code = $("#ohHi").html()
 var output = falafel(code, { loc: true }, function(node) {
   if(node.type === "Identifier") {
     if(node.parent && node.parent.type === "AssignmentExpression" && node.parent.left === node) {
-      node.parent.right.__hackedI = i
+      if(node.operator === "=") {
+        node.parent.right.__hackedI = i
+      } else {
+        node.parent.__hackedI = i
+      }
     } else if(node.parent && node.parent.type === "VariableDeclarator" && node.parent.id === node) {
       node.parent.init.__hackedI = i
     } else if(node.parent.type === "UpdateExpression") {
@@ -13,7 +17,9 @@ var output = falafel(code, { loc: true }, function(node) {
       node.update("__(" + i + ", " + node.source() + ")")
     }
     i++
-  }
+  } else {
+    console.log(node.type)
+  } 
   if(typeof node.__hackedI !== "undefined") {
     node.update("__(" + node.__hackedI + ", " + node.source() + ")") 
   }
@@ -22,19 +28,19 @@ var output = falafel(code, { loc: true }, function(node) {
 i = 0
 var htmlOutput = falafel(code, { loc: true }, function(node) {
   if(node.type == "Identifier") {
-    node.update('<span class="value" data-id="' + i + '">' + node.source() + '</span>')
+    var src = node.source()
+    node.update('<span class="value" data-id="' + i + '">' + src + '</span>')
     i++
   }
 })
 
-console.log(output.toString())
 eval('var __values = {}; __ = function(index, value) { return __values[index] = value }; ' + output)
 if(typeof $ != "undefined") {
   $(function() {
-    $(".code").html(htmlOutput.toString())
+    $(".code").html(htmlOutput.toString().replace(/\n/g, "<br />"))
     $(".value").each(function(i, element) {
       var el = $(element)
-      el.tooltip({ title: __values[parseInt(el.attr("data-id"))]})
+      el.tooltip({ title: __values[parseInt(el.attr("data-id"))].toString() || " "})
     })
   })
 } 
