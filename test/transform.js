b3a02edcb39a3a749857da5ca6d4f0eb5448adbf
Zach Smith <zach@amicushq.com>
2013-03-17 21:19:18 -0400

    A passing test

--- test/transform.js
+++ test/transform.js
@@ -1,9 +1,10 @@
 var transform = require("../lib/transform")
+  , expect = require("expect.js")
 
 describe("transform", function() {
   it("it should transform an assignment expression", function() {
     var code = "var x = 10"
-    var expected = "var x = _(0, 10)"
+    var expected = "var x = __(0, 10)"
     expect(transform(code)).to.be(expected)
   })
 })
