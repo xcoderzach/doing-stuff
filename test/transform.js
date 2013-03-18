30c0c55ef5ae9d834210a463ebc53e61f398237c
Zach Smith <zach@amicushq.com>
2013-03-17 21:47:49 -0400

    Don't over wrap

--- test/transform.js
+++ test/transform.js
@@ -2,14 +2,23 @@ var transform = require("../lib/transform")
   , expect = require("expect.js")
 
 describe("transform", function() {
+
   it("it should transform an variable declaration", function() {
     var code = "var x = 10"
     var expected = "var x = __(0, 10)"
     expect(transform(code)).to.be(expected)
   })
+
   it("it should transform an assignment expression", function() {
     var code = "x = 10"
     var expected = "x = __(0, 10)"
     expect(transform(code)).to.be(expected)
-  }) 
+  })
+   
+  it("it should transform an assignment expression", function() {
+    var code = "x = y"
+    var expected = "x = __(0, y)"
+    expect(transform(code)).to.be(expected)
+  })
+   
 })
