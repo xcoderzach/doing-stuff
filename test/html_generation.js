480a6642c79d1b9a8daf6c8ed5a67d20cdccb33e
Zach Smith <zach@amicushq.com>
2013-03-17 23:36:13 -0400

    Blocks workin

--- test/html_generation.js
+++ test/html_generation.js
@@ -4,9 +4,14 @@ var Transform = require("../lib/transform")
 function transformHtml(code) {
   var t = new Transform(code)
   t.executeCode()
-  return t.getHtml()
+  return t.getHTML()
 }
 
 describe("transform", function() {
-  
+  it("should html", function() {
+    var html = transformHtml("var x, y = 10; x = y + 5")
+    console.log(html)
+    html = transformHtml("for(var i = 0 ; i < 10 ; i++) { console.log(i) }")
+    console.log(html)
+  })
 })
